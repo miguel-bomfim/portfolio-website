6597a33f71f7d83500d659df0ba63250122aff05
Miguel Bomfim <miguelaubom@gmail.com>
2023-10-04 13:15:18 -0300

    Structuring home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { fetchHome } from "@/services/hygraphApi";
+import Image from "next/image";
 
 export default async function Home() {
   const homeData = await fetchHome();
@@ -6,13 +7,16 @@ export default async function Home() {
   return (
     <div>
       <nav>
-        <ul>
+        <ul className="flex items-center justify-center gap-6 p-6">
           <li>Portfolio</li>
           <li>Blog</li>
         </ul>
       </nav>
-      <h1 className="font-sans text-4xl m-4 ">{homeData.title}</h1>
-      <p>{homeData.bottomText}</p>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="font-sans text-4xl m-4">{homeData.title}</h1>
+        <p>{homeData.bottomText}</p>
+        <Image src={homeData.homeBackground.url} alt="" fill className="-z-10 opacity-70"/>
+      </div>
     </div>
   );
 }
